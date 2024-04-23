f03f8dc5b536b40757b466b8cb54c9bc8cd25df9
DoanQuy <123815148+DoanQuyyy@users.noreply.github.com>
2024-04-23 18:12:41 +0700

    update qlhd vs qltk

--- src/gui/GD_QuanLyHoaDon.java
+++ src/gui/GD_QuanLyHoaDon.java
@@ -1,7 +1,107 @@
 package gui;
 
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JScrollBar;
+import java.awt.Font;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
 
 public class GD_QuanLyHoaDon extends JPanel {
 
+	private static final long serialVersionUID = 1L;
+	private JTable table;
+	private JTextField textField;
+	private JTable table_1;
+
+	/**
+	 * Launch the application.
+	 */
+	/**
+	 * Create the frame.
+	 */
+	public GD_QuanLyHoaDon() {
+		setBackground(new Color(217, 217, 217));
+	    setSize(1140, 865);
+		setBorder(new EmptyBorder(5, 5, 5, 5));
+		setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Quản lí hóa đơn");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.PLAIN, 30));
+		lblNewLabel.setBounds(460, 28, 241, 45);
+		add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Danh sách hóa đơn");
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 23));
+		lblNewLabel_1.setBounds(96, 113, 291, 24);
+		add(lblNewLabel_1);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(96, 168, 939, 241);
+		add(scrollPane);
+		
+		
+		table = new JTable();
+		scrollPane.setViewportView(table);
+		String header[]= {
+				"Mã Hóa Đơn","Mã Nhân Viên", "Tên Khách Hàng", "Ngày Lập Hóa Đơn", "Số Lượng Vé","Số Điện Thoại", "Tổng Tiền"};
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},header
+		));
+		table.getTableHeader().setFont(new Font("Arial", Font.BOLD, 15));
+      	table.setRowHeight(25);
+      	table.setBackground(new Color(217,217,217));
+      	
+		JComboBox comboBox = new JComboBox();
+		comboBox.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Tìm kiếm theo tên", "Tìm kiếm theo ngày "}));
+		comboBox.setBounds(96, 452, 169, 33);
+		add(comboBox);
+		
+		textField = new JTextField();
+		textField.setBounds(299, 452, 530, 33);
+		add(textField);
+		textField.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Tìm");
+		btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 15));
+		btnNewButton.setBounds(909, 452, 126, 33);
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setBackground(Color.black);
+		btnNewButton.setBorderPainted(false);
+		add(btnNewButton);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Chi tiết hóa đơn");
+		lblNewLabel_1_1.setFont(new Font("Times New Roman", Font.PLAIN, 23));
+		lblNewLabel_1_1.setBounds(96, 513, 183, 24);
+		add(lblNewLabel_1_1);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(96, 560, 937, 216);
+		add(scrollPane_1);
+		
+		table_1 = new JTable();
+		scrollPane_1.setViewportView(table_1);
+		table_1.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"M\u00E3 V\u00E9", "M\u00E3 H\u00F3a \u0110\u01A1n", "G\u00EDa V\u00E9"
+			}
+		));
+		table_1.getTableHeader().setFont(new Font("Arial", Font.BOLD, 15));
+      	table_1.setRowHeight(25);
+      	table_1.setBackground(new Color(217,217,217));
+	}
 }
--- src/gui/GD_ThongKe.java
+++ src/gui/GD_ThongKe.java
@@ -3,19 +3,19 @@ package gui;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import java.awt.Font;
+import com.toedter.calendar.JDateChooser;
 import javax.swing.JButton;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
-
-import com.toedter.calendar.JDateChooser;
-
 import javax.swing.JScrollPane;
-
+import java.awt.Color;
+import javax.swing.UIManager;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GD_ThongKe extends JPanel {
 
 	private static final long serialVersionUID = 1L;
-	private JTable table;
 
 	/**
 	 * Create the panel.
@@ -23,23 +23,23 @@ public class GD_ThongKe extends JPanel {
 	public GD_ThongKe() {
 		setSize(1140, 865);
 		setLayout(null);
-		
+		setBackground(new Color(217, 217, 217));
 		JLabel lblNewLabel_1 = new JLabel("Từ Ngày ");
-		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel_1.setBounds(114, 137, 81, 25);
+		lblNewLabel_1.setFont(new Font("Times New Roman", Font.PLAIN, 24));
+		lblNewLabel_1.setBounds(114, 137, 90, 25);
 		add(lblNewLabel_1);
 		
 		JLabel lblNewLabel = new JLabel("Báo Cáo Thống Kê");
-		lblNewLabel.setBounds(428, 32, 220, 33);
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 27));
+		lblNewLabel.setBounds(428, 32, 253, 33);
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 30));
 		add(lblNewLabel);
 		
 		JDateChooser dateChooser = new JDateChooser();
-		dateChooser.setBounds(220, 137, 197, 25);
+		dateChooser.setBounds(239, 137, 197, 25);
 		add(dateChooser);
 		
 		JLabel lblNewLabel_2 = new JLabel("Đến Ngày");
-		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel_2.setFont(new Font("Times New Roman", Font.PLAIN, 24));
 		lblNewLabel_2.setBounds(714, 137, 99, 25);
 		add(lblNewLabel_2);
 		
@@ -48,34 +48,51 @@ public class GD_ThongKe extends JPanel {
 		add(dateChooser_1);
 		
 		JButton btnNewButton = new JButton("Xem");
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBackground(Color.black);
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setFont(new Font("Times New Roman", Font.BOLD, 20));
 		btnNewButton.setBounds(491, 194, 73, 25);
+		btnNewButton.setBorderPainted(false);
 		add(btnNewButton);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(114, 249, 929, 454);
-		add(scrollPane);
-		
-		table = new JTable();
-		scrollPane.setViewportView(table);
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-				{null, null, null, null},
-			},
-			new String[] {
-				"Tên Phim", "Số Suất Chiếu", "Số Vé Bán Ra", "Doanh Thu"
-			}
-		));
-		
 		JLabel lblNewLabel_3 = new JLabel("Tổng Doanh Thu");
-		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		lblNewLabel_3.setBounds(114, 745, 166, 25);
+		lblNewLabel_3.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		lblNewLabel_3.setBounds(114, 763, 166, 25);
 		add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_3_1 = new JLabel("100000000");
+		lblNewLabel_3_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel_3_1.setBounds(877, 763, 166, 25);
+		add(lblNewLabel_3_1);
+		
+		
+		DefaultTableModel dataModel = new DefaultTableModel();
+      	String headers[] = {"Tên Phim", "Số Suất Chiếu", "Số Vé Bán Ra", "Doanh Thu"};
+      	dataModel = new DefaultTableModel(headers,0);
+      	JTable table = new JTable(new DefaultTableModel(
+      		new Object[][] {
+      			{null, null, null, null},
+      			{null, null, null, null},
+      			{null, null, null, null},
+      			{null, null, null, null},
+      		},
+      		new String[] {
+      			"T\u00EAn Phim", "S\u1ED1 Su\u1EA5t Chi\u1EBFu", "S\u1ED1 V\u00E9 B\u00E1n Ra", "Doanh Thu"
+      		}
+      	));
+      	table.getTableHeader().setFont(new Font("Arial", Font.BOLD, 20));
+      	table.setRowHeight(25);
+      	table.setBackground(new Color(217,217,217));
+      	dataModel.addRow(new Object[]{"Phim Hài Hước", "10", "1200", "10000000"});
+      	dataModel.addRow(new Object[]{"Phim Hành Động", "28", "4500", "40000000"});
+        table.setModel(dataModel);
+        JScrollPane scroll = new JScrollPane(table);
+      	scroll.setBackground(new Color(217,217,217));
+      	scroll.setBounds(114, 263, 929, 458);
+      	add(scroll);
 	}
 }
